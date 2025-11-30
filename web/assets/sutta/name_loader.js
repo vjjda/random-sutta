62b1a648b6c46ab8861089f643230c5ada512c58
Vijjo <vjjdaya@gmail.com>
2025-11-30 22:42:49 +0700

    update

--- web/assets/sutta/name_loader.js
+++ web/assets/sutta/name_loader.js
@@ -3,54 +3,38 @@
 (function() {
     const files = [
   "an-name.js",
-  "arv-name.js",
-  "avs-name.js",
-  "bv-name.js",
-  "cnd-name.js",
-  "cp-name.js",
-  "d-name.js",
-  "da-name.js",
-  "da-ot-name.js",
-  "dhp-name.js",
-  "divy-name.js",
   "dn-name.js",
-  "gdhp-name.js",
-  "iti-name.js",
-  "ja-name.js",
-  "kp-name.js",
-  "lal-name.js",
-  "lzh-dharani-name.js",
-  "lzh-ssnp-name.js",
-  "lzh-svk-name.js",
-  "ma-name.js",
-  "mil-name.js",
+  "kn/bv-name.js",
+  "kn/cnd-name.js",
+  "kn/cp-name.js",
+  "kn/dhp-name.js",
+  "kn/iti-name.js",
+  "kn/ja-name.js",
+  "kn/kp-name.js",
+  "kn/mil-name.js",
+  "kn/mnd-name.js",
+  "kn/ne-name.js",
+  "kn/pe-name.js",
+  "kn/ps-name.js",
+  "kn/pv-name.js",
+  "kn/snp-name.js",
+  "kn/tha-ap-name.js",
+  "kn/thag-name.js",
+  "kn/thi-ap-name.js",
+  "kn/thig-name.js",
+  "kn/ud-name.js",
+  "kn/vv-name.js",
   "mn-name.js",
-  "mnd-name.js",
-  "ne-name.js",
-  "pdhp-name.js",
-  "pe-name.js",
-  "ps-name.js",
-  "pv-name.js",
-  "sf-name.js",
-  "sn-name.js",
-  "snp-name.js",
-  "tha-ap-name.js",
-  "thag-name.js",
-  "thi-ap-name.js",
-  "thig-name.js",
-  "ud-name.js",
-  "uv-name.js",
-  "uvs-name.js",
-  "vv-name.js",
-  "ybs-name.js"
+  "sn-name.js"
 ];
-    // UPDATED: Script này ở assets/sutta/name_loader.js
-    // Data files ở assets/sutta/names/*.js
-    // -> thay 'name_loader.js' thành 'names/'
+    // Script này nằm tại assets/sutta/name_loader.js
+    // basePath sẽ trỏ vào assets/sutta/names/
     const basePath = document.currentScript.src.replace('name_loader.js', 'names/');
     
     files.forEach(file => {
         const script = document.createElement('script');
+        // file đã chứa relative path (vd: "kn/dhp-name.js" hoặc "mn-name.js")
+        // nên nối vào basePath là chuẩn: ".../names/kn/dhp-name.js"
         script.src = basePath + file;
         script.async = false;
         document.head.appendChild(script);
