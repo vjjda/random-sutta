c3fa3efa8e70070755ae1f22ba20e9aa472636e7
Vijjo <vjjdaya@gmail.com>
2025-12-03 17:28:03 +0700

    v1.2.2

--- web/sw.js
+++ web/sw.js
@@ -1,7 +1,7 @@
 // Path: web/sw.js
 
 // Tên cache mới (Nên bump version khi deploy)
-const CACHE_NAME = "sutta-reader-cache-v1.2.1";
+const CACHE_NAME = "sutta-reader-cache-v1.2.2";
 
 const SUTTA_DATA_FILES = [
   "an.js",
