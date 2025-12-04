adc610239b1eb1907fed7ecb88f25195cd0bfd46
Vijjo <vjjdaya@gmail.com>
2025-12-04 08:43:04 +0700

    v1.1.6

--- web/sw.js
+++ web/sw.js
@@ -1,7 +1,7 @@
 // Path: web/sw.js
 
 // Tên cache mới (Nên bump version khi deploy)
-const CACHE_NAME = "sutta-reader-cache-v17d0e91";
+const CACHE_NAME = "sutta-reader-cache-v1.2.6";
 
 const SUTTA_DATA_FILES = [
   "./assets/books/abhidhamma/ds_book.js",
