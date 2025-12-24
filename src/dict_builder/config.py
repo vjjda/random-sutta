13a0a917d765a7fbc18ef1bd8e17f6b789717b24
Vijjo <vjjdaya@gmail.com>
2025-12-25 01:48:57 +0700

    config: disable book filter (select all books) by setting EBTS_BOOKS to ['']

--- src/dict_builder/config.py
+++ src/dict_builder/config.py
@@ -11,16 +11,11 @@ class BuilderConfig:
 
     # --- Settings ---
     USE_COMPRESSION: bool = False 
+    EBTS_BOOKS: List[str] = [""]
 
-    EBTS_BOOKS: List[str] = [
-        "vin1", "vin2", "vin3", "vin4",
-        "dn1", "dn2", "dn3",
-        "mn1", "mn2", "mn3",
-        "sn1", "sn2", "sn3", "sn4", "sn5",
-        "an1", "an2", "an3", "an4", "an5", 
-        "an6", "an7", "an8", "an9", "an10", "an11",
-        "kn1", "kn2", "kn3", "kn4", "kn5", "kn8", "kn9",
-    ]
+    # EBTS_BOOKS: List[str] = [
+    #     "an", "mn", "dn", "sn", "thag", "thig", "iti", "snp", "vbh", "pv", "ps", "ja", "tha-ap", "thi-ap", "ud", "dhp", "mnd", "dnd", "vv", "ds", "dt"
+    # ]
 
     def __init__(self, mode: str = "mini"):
         self.mode = mode
