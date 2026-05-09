2235ede55195a60e1b837f2388576dc85eb10a5f
Vijjo <vjjdaya@gmail.com>
2026-05-09 06:40:37 +0545

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/random-sutta/sw.js', { scope: '/random-sutta/' })})}
